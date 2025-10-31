83195c253c36d38b88200ac6d1235a9478a89384
iceyatom <kk6uzh2@gmail.com>
2025-10-31 15:16:53 -0700

    Implemented seed file to populate test catalog data into the database. Ran ESLint and Prettier to ensure code quality and consistency. (#14)

--- nova_project/src/app/catalog/page.tsx
+++ nova_project/src/app/catalog/page.tsx
@@ -2,11 +2,36 @@ import ItemCard from "../components/ItemCard";
 
 function getItems() {
   return [
-    { id: "1", name: "Fillerilleriller", imageUrl: "/FillerImage.png", description: "desciption" },
-    { id: "2", name: "Filler", imageUrl: "/FillerImage.png", description: "desciption" },
-    { id: "3", name: "Filler", imageUrl: "/FillerImage.png", description: "desciption" },
-    { id: "4", name: "Filler", imageUrl: "/FillerImage.png", description: "desciption" },
-    { id: "5", name: "Filler",  imageUrl: "/FillerImage.png", description: "desciption" },
+    {
+      id: "1",
+      name: "Fillerilleriller",
+      imageUrl: "/FillerImage.png",
+      description: "desciption",
+    },
+    {
+      id: "2",
+      name: "Filler",
+      imageUrl: "/FillerImage.png",
+      description: "desciption",
+    },
+    {
+      id: "3",
+      name: "Filler",
+      imageUrl: "/FillerImage.png",
+      description: "desciption",
+    },
+    {
+      id: "4",
+      name: "Filler",
+      imageUrl: "/FillerImage.png",
+      description: "desciption",
+    },
+    {
+      id: "5",
+      name: "Filler",
+      imageUrl: "/FillerImage.png",
+      description: "desciption",
+    },
   ];
 }
 
